8f14e0922d100f8509ed6b4a3120f1e2ba91f3a6
Yusuf Adepoju <yusufadepoju23@gmail.com>
2026-05-11 14:24:06 +0100

    correct CI/CD pipeline

---
+++ .github/workflows/deploy.ml
@@ -0,0 +1,28 @@
+name: Flask CI/CD Deploy
+
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v4
+
+      - name: Deploy to EC2
+        uses: appleboy/ssh-action@v1.0.3
+        with:
+          host: ${{ secrets.EC2_HOST }}
+          username: ${{ secrets.EC2_USER }}
+          key: ${{ secrets.EC2_KEY }}
+          script: |
+            cd ~/Portfolio_website
+            git pull origin main
+            source venv/bin/activate
+            pip install -r requirements.txt
+            pkill -f app.py || true
+            nohup python app.py > app.log 2>&1 &
